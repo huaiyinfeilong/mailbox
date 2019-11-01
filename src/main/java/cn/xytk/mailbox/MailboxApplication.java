b5515968d9014798e58d189a8049292b5bb50ee6
huaiyinfeilong <huaiyinfeilong@163.com>
2019-11-01 21:09:33 +0800

    '初始化存储库。'

---
+++ src/main/java/cn/xytk/mailbox/MailboxApplication.java
@@ -0,0 +1,13 @@
+package cn.xytk.mailbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/xytk/mailbox/MailboxApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.xytk.mailbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
